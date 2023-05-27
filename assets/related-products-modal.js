6353fd74ccbd211dd7e65d348113b69b2c5a5196
Andrii.Piasetskyi <andrew.piasetskyi.dev@gmail.com>
2023-05-27 14:56:39 +0300

    REF1-8:ADD: Section module script, creating new modal instanse

---
+++ assets/related-products-modal.js
@@ -0,0 +1,84 @@
+class RelatedProductsModal {
+    constructor(options) {
+        this.sectionId = options.sectionId;
+        this.element = document.querySelector(`#${this.sectionId}`);
+        this.showDelay = options.showDelay * 1000; // Convert to milliseconds
+        this.showOnlyOncePerCustomer = options.showOnlyOncePerCustomer;
+        this.classes = {
+            hiddenClass: "hidden",
+            overlay: "related-products-modal__overlay",
+            actionBtn: "related-products-modal__btn",
+        };
+
+        // Check if the modal should be shown only once per customer
+        if (this.showOnlyOncePerCustomer) {
+            const cookieName = "relatedProductsModal";
+
+            // Check if the cookie exists
+            if (this.getCookie(cookieName)) {
+                return;
+            }
+
+            // Set the cookie
+            this.setCookie(cookieName, "true");
+        }
+
+        this.init();
+    }
+
+    handleEvents() {
+        let overlay = this.element.querySelector(`.${this.classes.overlay}`);
+        let actionBtn = this.element.querySelector(
+            `.${this.classes.actionBtn}`
+        );
+
+        overlay.addEventListener("click", (e) => {
+            if (e.target === overlay) {
+                this.hide(overlay, this.classes.hiddenClass);
+            }
+        });
+
+        actionBtn.addEventListener("click", (e) => {
+            console.log("ADD to cart");
+        });
+    }
+
+    show(element, hiddenClass, delay) {
+        setTimeout(() => {
+            if (element.classList.contains(hiddenClass)) {
+                element.classList.remove(hiddenClass);
+            }
+        }, delay);
+    }
+
+    hide(element, hiddenClass) {
+        if (!element.classList.contains(hiddenClass)) {
+            element.classList.add(hiddenClass);
+        }
+    }
+
+    setCookie(name, value) {
+        document.cookie = `${name}=${value}; path=/`;
+    }
+
+    getCookie(name) {
+        // Implementation for getting a cookie
+        // Replace this with your actual logic
+        const cookies = document.cookie.split(";");
+
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+
+            if (cookie.startsWith(name + "=")) {
+                return cookie.substring(name.length + 1);
+            }
+        }
+
+        return null;
+    }
+
+    init() {
+        this.show(this.element, this.classes.hiddenClass, this.showDelay);
+        this.handleEvents();
+    }
+}
